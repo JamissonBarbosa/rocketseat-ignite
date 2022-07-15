eb540f98ff80a5cd60160f5fd3f189c9bfe84752
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2022-07-15 15:23:27 -0300

    atualizando repos

---
+++ react-js/fundamentos-react-js/src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post"
+import "./styles.css"
+
+function App() {
+  return (
+    <Post 
+      author="Joao"
+      content="Paragrafo falando do meu post."
+    />
+  )
+}
+
+export default App
